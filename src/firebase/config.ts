f23805bfc4982aee1c7a280c476bb7f20573c44e
De Leon, Edman Lenard V. <edman.deleon@neu.edu.ph>
2026-03-16 03:35:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3548583424-ec0c5",
+  "appId": "1:404021608177:web:9e7d3387248ab789cbf350",
+  "apiKey": "AIzaSyBIefegAmrHj4IMrrGo98VqoIgpw5F3pLU",
+  "authDomain": "studio-3548583424-ec0c5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "404021608177"
+};
